3c439aa5edf72ce824b5274e80e670ae0d0a892e
sandrine <sandrinezocca@gmail.com>
2019-05-09 13:53:31 +0200

    import javascript

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
 import './App.css';
+import { sampleText } from './sampleText';
+
 
 class App extends Component {
-  state = {}
+
+  constructor(props) {
+    super(props);
+    this.state = { 
+      text: sampleText
+     }
+  }
+
   render() {
     return (
       <div className="container">
         <h1>Markdown Editor</h1>
         <div className="row">
           <div className="col-sm-6">
-            <textarea className="form-control" rows="30"></textarea>
+            <textarea 
+            className="form-control" 
+            rows="30"
+            value={ this.state.text }
+            />
           </div>
           <div className="col-sm-6 result">
-            <h2>Résultat</h2>
+            <div>
+              { this.state.text }
+            </div>
           </div>
         </div>
       </div>
