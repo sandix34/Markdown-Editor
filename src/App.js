e6936ac4042c3a1ef5f01f47748f778206881349
sandrine <sandrinezocca@gmail.com>
2019-05-09 13:35:27 +0200

    set up

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>markdown</h1>
-    </div>
-  );
+class App extends Component {
+  state = {}
+  render() {
+    return (
+      <div className="container">
+        <h1>Markdown Editor</h1>
+        <div className="row">
+          <div className="col-sm-6">
+            <textarea className="form-control" rows="30"></textarea>
+          </div>
+          <div className="col-sm-6 result">
+            <h2>Résultat</h2>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
